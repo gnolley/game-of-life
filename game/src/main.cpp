36f1bd238db507eb23d6133ee78010515e4e66c0
Thomas Knowles <thomasa.knowles@gmail.com>
2025-09-20 21:32:19 +1000

    feat: add simulation to main

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -2,6 +2,19 @@
 #include "FileReader.h"
 #include <CLI/CLI.hpp>
 
+#include "Simulation.h"
+
+void simulation_loop(GoL::Simulation& simulation, int max_ticks)
+{
+	int ticks = 0;
+	while (ticks < max_ticks && simulation.get_current_frame().cell_count() > 0)
+	{
+		++ticks;
+		simulation.tick();
+	}
+
+	std::cout << std::format("Finished simulating after {} ticks\n", ticks);
+}
 
 int main(int argc, char** argv)
 {
@@ -10,6 +23,9 @@ int main(int argc, char** argv)
 
 	std::string life_file = "default";
 	gol_app.add_option("-f,--file", life_file, "Path to the life 1.06 file you would like to load.");
+
+	int num_ticks = 100;
+	gol_app.add_option("-t, --ticks", num_ticks, "Number of ticks to simulate. Default 100.");
 
 	CLI11_PARSE(gol_app, argc, argv);
 
@@ -30,5 +46,8 @@ int main(int argc, char** argv)
 	}
 
 	GoL::FileReader::LoadFile(frame, life_file_path);
-	std::cout << std::format("Loaded snapshot with {} cells.", frame.cell_count());
+	std::cout << std::format("Loaded snapshot with {} cells.\n", frame.cell_count());
+
+	GoL::Simulation simulation(std::move(frame));
+	simulation_loop(simulation, num_ticks);
 }
